ff6bcf41208e28c01ac881cdb68816a115c4ec69
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2016-12-23 19:21:22 +0000

    Arxiv library expanded. New functions for parsing the response of the Arxiv, and for creating the answer for the user

--- Library/arxiv_lib.py
+++ Library/arxiv_lib.py
@@ -1,11 +1,127 @@
 from customised_exceptions import NoArgumentError, GetRequestError, UnknownError
 import requests
 import feedparser
-import urllib
+
+# Review response, it will review the dictionary obtained from parse_response
+# and pass the following information for each entries:
+#
+# - title
+# - authors
+# - year
+# - link
+#
+# Only these information are passed since they will go to the telegram app.
+# The return argument is a list of dictionary with these entries
+
+def review_response(dictionary):
+
+	results_list = []
+
+	if not isinstance(dictionary, dict):
+		raise TypeError('The argument passed is not a dictionary.')
+
+	try:
+		if not isinstance(dictionary['entries'], list):
+			raise TypeError('The field entries is corrupted.')
+	except KeyError:
+		raise NoArgumentError('No entries have been found during the search.')
+
+	for entry in dictionary['entries']:
+
+		if not isinstance(entry,dict):
+			raise TypeError('One of the entries is corrupted.')
+
+		# Only write the important data in the dictionary
+		element = {'title' : one_line_title(entry),
+				   'authors' : compact_authors(entry),
+				   'year' : find_year(entry),
+				   'link' : is_field_there(entry, 'link')}
+		
+		# Check whether all field in the element are None
+		is_empty = element['title'] == None and element['authors'] == None and element['year'] == None and element['link'] == None
+
+		if not is_empty:
+			results_list.append(element)
+
+	if len(results_list) == 0:
+		raise NoArgumentError('No entries have been found during the search.')
+	
+	return results_list
+
+# This function is needed for the review_response.
+# It checks that the dictionary has something associated to the key, and if not, it returns None
+def is_field_there(dictionary, key):
+
+	try:
+		return dictionary[key]
+	except:
+		return None
+
+# This function is needed for the review_response.
+# It modifies the title so that any \n is absent
+def one_line_title(dictionary):
+
+	title = is_field_there(dictionary, 'title')
+
+	if isinstance(title, unicode):
+		title = title.replace(u'\n',u'')
+		return title
+	else:
+		return None
+
+# This function is needed for the review_response.
+# It unifies the name of the authors in a single one, and return a unicode string (if there are some authors)
+def compact_authors(dictionary):
+
+	authors_list = is_field_there(dictionary, 'authors')
+	authors_string = unicode( '' , "utf-8")
+
+	if isinstance(authors_list, list):
+		for author in authors_list:
+			author_name = is_field_there(author, 'name')
+			if isinstance(author_name, unicode):
+				authors_string = authors_string + author_name + unicode( ', ' , "utf-8")
+	else:
+		return None
+
+	# Check if we have something in the authors string
+	if len(authors_string) == 0:
+		return None
+	else:
+		authors_string = authors_string[: -2]
+		return authors_string
+
+# This function is needed for the review_response.
+# For a given date, it returns the year.
+def find_year(dictionary):
+
+	date = is_field_there(dictionary, 'date')
+
+	if isinstance(date, unicode) and len(date) > 3:
+		date = date[0:4]
+		return date
+	else:
+		return None
+
+# Parse the response. It modifies the response obtained by the request library
+# making it a rawdata (string). Then, it parse it using parsefeed. It returns
+# a dictionary.
+
+def parse_response(response):
+	
+	if not isinstance(response, requests.models.Response):
+		raise TypeError('The argument passed is not a Response object.')
+
+	rawdata = response.text
+
+	parsed_response = feedparser.parse(rawdata)
+
+	return parsed_response
 
 # Request function to comunicate with the arxiv and download the informations
 # It takes a string (the link) as argument
 # Check for the main connection errors.
+# Return the response
 
 def request_to_arxiv(arxiv_search_link):
 
@@ -31,6 +147,7 @@ def request_to_arxiv(arxiv_search_link):
 		return response
 
 # Advanced search the arxiv.
+#
 # As parameter, it takes the following parameters:
 #
 # au			Author
@@ -43,8 +160,9 @@ def request_to_arxiv(arxiv_search_link):
 # id			Id
 # arxiv_search_link	The arxiv link where making the query
 # 
-# And it make a query to the arxive to search for these parameters.
-# One do not need to specifies all the variables.
+# And it make the link for the request to arxiv, which will be passed
+# to the request to arxiv.
+# 
 # If a variable is not a string, it is not included in the function,
 # But no exception is raised. Exception is raised only is the search
 # is empty. After the query is done, the response is returned.
@@ -79,29 +197,39 @@ def advanced_search(author, title, abstract, comment, jref, category, rnum, iden
 	# Remove last connector fromt the search
 	arxiv_search_link = arxiv_search_link[: - len(connector) ]
 
-	# Make the request
-	try:
-		response = request_to_arxiv(arxiv_search_link)
-	except requests.exceptions.InvalidSchema as invalid_schema:
-		raise invalid_schema
-	except requests.exceptions.MissingSchema as missing_schema:
-		raise missing_schema
-	except GetRequestError:
-		raise GetRequestError('Get from arxiv failed. Might be connection problem')
-	except requests.exceptions.HTTPError as connection_error:
-		raise connection_error
-	except TypeError:
-		raise TypeError('The argument passed is not a string.')
-	except:
-		raise UnknownError('During the request to Arxiv something went wrong.')
-	else:
-		return response
+	return arxiv_search_link
 
 # The simple search on the arxiv.
+# 
 # It takes the argument of the search, which is searched in the all fields,
-# such as title, author, etc. The argument of this function has to be a string.
-# If nothing is passed, the search is not perfomed.
+# such as title, author, etc. The argument of this function has to be a string,
+# or a list of strings. If nothing is passed, the search is not perfomed.
+# Retruns the link for the call to arxiv
 
-def simple_search(word):
+def simple_search(words, arxiv_search_link):
+
+	# Initialising constant values for the search
+	connector = '+AND+'
+	length_check = len(arxiv_search_link)
+	key = 'all:'
+
+	# If the argument is a list of words, iterate over it
+	if isinstance(words, list):
+		for word in words:
+			if isinstance(word, str):
+				arxiv_search_link += key + word + connector
+
+	# If it is a single string, add it without iterations
+	elif isinstance(words, str):
+		arxiv_search_link += key + words + connector
+
+	# Check that search_def is not empty. In that case, return error
+	if len(arxiv_search_link) == length_check:
+		raise NoArgumentError('No arguments have been provided to the search.')
+
+	# Remove last connector fromt the search
+	arxiv_search_link = arxiv_search_link[: - len(connector) ]
+
+	return arxiv_search_link
 
 	return 0	
--- Library/test_arxiv_lib.py
+++ Library/test_arxiv_lib.py
@@ -1,26 +1,11 @@
 from nose.tools import assert_raises, assert_equal
-from arxiv_lib import request_to_arxiv, advanced_search
+import arxiv_lib as al
 from customised_exceptions import NoArgumentError, GetRequestError
-from subprocess import DEVNULL, STDOUT, check_call
+from subprocess import check_call
 import requests
 import time
 
-# When anything but a string is passed to the request_to_arxiv it needs to rise error (TypeError)
-def test_request_to_arxiv_no_string():
-
-	possible_arguments = [1, 1., [1, 'a'], False, {'a' : 1}]
-
-	for arg in possible_arguments:
-		with assert_raises(TypeError):
-			request_to_arxiv(arg)
-
-# When the link is not in the correct format (MissingSchema)
-def test_request_to_arxiv_wrong_link_format():
-
-	link = 'wrong_link_format'
-
-	with assert_raises(requests.exceptions.MissingSchema):
-		request_to_arxiv(link)
+# ADVANCED SEARCH TESTS
 
 # When no arguments are passed, we need to raise error (NoArgumentError)
 def test_advanced_search_no_arguments():
@@ -28,49 +13,352 @@ def test_advanced_search_no_arguments():
 	link = 'http://export.arxiv.org/api/query?search_query='	
 
 	with assert_raises(NoArgumentError):
-		advanced_search(False, False, False, False, False, False, False, False, link)
-
-# When the link is not in the correct format (InvalidSchema)
-def test_advanced_search_wrong_link_format():
-
-	link = 'wrong_link_format'
-
-	with assert_raises(requests.exceptions.InvalidSchema):
-		advanced_search('sparaciari_c', False, False, False, False, False, False, False, link)
-		
-# When there is not Internet Connection (GetRequestError):
-def test_advanced_search_no_connection():
-	
-	link = 'http://export.arxiv.org/api/query?search_query='	
-
-	# We simulate the absence of internet connection by turning off the wifi
-	check_call(['networksetup', '-setairportpower', 'airport', 'off'], stdout=DEVNULL, stderr=STDOUT)
-
-	with assert_raises(GetRequestError):
-		advanced_search('sparaciari_c', False, False, False, False, False, False, False, link)
-
-	# We turn it on stright after, waiting 15 seconds to be sure we are up and running
-	check_call(['networksetup', '-setairportpower', 'airport', 'on'], stdout=DEVNULL, stderr=STDOUT)
-	time.sleep(5)
+		al.advanced_search(False, False, False, False, False, False, False, False, link)
 
 # When the response from arxiv is wrong (HTTPError)
 def test_advanced_search_status_code():
 
-	link = 'http://export.arxiv.org/apii/query?search_query='	
+	link = 'http://export.arxiv.org/apii/query?search_query=au:sparaciari_c'	
 
 	with assert_raises(requests.exceptions.HTTPError):
-		advanced_search('sparaciari_c', False, False, False, False, False, False, False, link)
+		al.request_to_arxiv(link)
 
-# When everything goes well.
+# When the request to arxiv goes well
 def test_advanced_search_correct():
 
 	link = 'http://export.arxiv.org/api/query?search_query='
 	test_file = 'text_response_test_advanced_search.txt'
 
-	response = advanced_search('sparaciari_c', 'kullback', False, False, False, False, False, False, link).text
+	search_link = al.advanced_search('sparaciari_c', 'kullback', False, False, False, False, False, False, link)
+	response = al.request_to_arxiv(search_link).text
 
 	with open(test_file, 'r') as f:
 		expected = f.read()
 
 	# We remove the first few information, containing the current date.
 	assert_equal(response[504:], expected[504:], "The obtained response is different from the expected one")
+
+# SIMPLE SEARCH TESTS
+
+# When no arguments are passed, we need to raise error
+def test_simple_search_no_arguments():
+
+	link = 'http://export.arxiv.org/api/query?search_query='	
+
+	with assert_raises(NoArgumentError):
+		al.simple_search(False, link)
+
+# When the simple search returns the correct link for a string argument
+def test_simple_search_correctness_str():
+
+	link = 'http://export.arxiv.org/api/query?search_query='
+	correct_link = 'http://export.arxiv.org/api/query?search_query=all:electron'
+
+	obtained_link = al.simple_search('electron', link)
+
+	assert_equal(obtained_link, correct_link, "The obtained link is different from the expected one")
+
+# When the simple search returns the correct link for a list argument
+def test_simple_search_correctness_list():
+
+	link = 'http://export.arxiv.org/api/query?search_query='
+	correct_link = 'http://export.arxiv.org/api/query?search_query=all:electron+AND+all:proton'
+
+	obtained_link = al.simple_search(['electron', 'proton'], link)
+
+	assert_equal(obtained_link, correct_link, "The obtained link is different from the expected one")
+
+# REQUEST TO ARXIV TESTS
+
+# When anything but a string is passed to the al.request_to_arxiv it needs to rise error (TypeError)
+def test_request_to_arxiv_no_string():
+
+	possible_arguments = [1, 1., [1, 'a'], False, {'a' : 1}]
+
+	for arg in possible_arguments:
+		with assert_raises(TypeError):
+			al.request_to_arxiv(arg)
+
+# When the link is not in the correct format (MissingSchema)
+def test_request_to_arxiv_wrong_link_format():
+
+	link = 'wrong_link_format'
+
+	with assert_raises(requests.exceptions.MissingSchema):
+		al.request_to_arxiv(link)
+
+# When there is not Internet Connection (GetRequestError) - Need administrator privileges to work:
+# def test_request_to_arxiv_no_connection():
+	
+# 	link = 'http://export.arxiv.org/api/query?search_query=au:sparaciari_c'	
+
+# 	# We simulate the absence of internet connection by turning off the wifi
+# 	# For Windows use : netsh interface set interface "Wi-Fi" disabled
+# 	check_call('netsh interface set interface "Wi-Fi" disabled',shell=True)
+
+# 	with assert_raises(GetRequestError):
+# 		al.request_to_arxiv(link)
+
+# 	# We turn it on stright after, waiting 15 seconds to be sure we are up and running
+# 	# For Windows use : netsh interface set interface "Wi-Fi" enabled
+# 	check_call('netsh interface set interface "Wi-Fi" enabled',shell=True)
+# 	time.sleep(10)
+
+# PARSE RESPONSE TESTS
+
+# when the argument of parse response is not a requests.Respones, need to return error
+def test_parse_response_wrong_argument():
+
+	possible_arguments = [1, 1., [1, 'a'], False, {'a' : 1}]
+
+	for arg in possible_arguments:
+		with assert_raises(TypeError):
+			al.parse_response(arg)
+
+# when the parse response work fine
+def test_parse_response_correct():
+
+	link = 'http://export.arxiv.org/api/query?search_query=all:electron+AND+all:proton'
+	test_file = 'text_parsing_test.txt'
+
+	response = al.request_to_arxiv(link)
+	parsed_response = al.parse_response(response)
+
+	# We select the abstract of the first paper downloaded
+	abstract_obtained = parsed_response['entries'][0]['summary_detail']['value']
+
+	with open(test_file, 'r') as f:
+		expected_abstract = unicode( f.read() , "utf-8")
+	
+	assert_equal(abstract_obtained, expected_abstract, "The obtained response is different from the expected one")
+
+	return 0
+
+# REVIEW RESPONSE TESTS
+
+# when the argument of review response is not a dictionary, need to return error
+def test_review_response_wrong_argument():
+
+	possible_arguments = [1, 1., [1, 'a'], False]
+
+	for arg in possible_arguments:
+		with assert_raises(TypeError):
+			al.review_response(arg)
+
+# when the dictionary does not have the field 'entries'
+def test_review_response_no_entries():
+
+	dictionary = {'a' : 1 , 'b' : 'hi'}
+
+	with assert_raises(NoArgumentError):
+		al.review_response(dictionary)
+
+# when the dictionary has the field 'entries', but is not a list
+def test_review_response_entries_no_list():
+
+	dictionary = {'a' : 1 , 'b' : 'hi' , 'entries' : 'this is not a list'}
+
+	with assert_raises(TypeError):
+		al.review_response(dictionary)
+
+# when the dictionary has the field 'entries', it is a list, but not a list of dictionaries
+def test_review_response_entries__list_no_dictionary():
+
+	dictionary = {'a' : 1 , 'b' : 'hi' , 'entries' : [1,2,3]}
+
+	with assert_raises(TypeError):
+		al.review_response(dictionary)
+
+# when the dictionary has the field 'entries', and is a list of dictionaries, and one entry is correct
+def test_review_response_correct():
+
+	dictionary = {'a' : 1 , 'b' : 'hi' ,
+				  'entries' : [{'noise' : True,
+				  				'title' : u'Nice Title',
+				  				'animal' : 'Dog',
+				  				'authors' : [{'name' : u'Carlo'}],
+				  				'date' : u'1992-05-12',
+				  				'link' : 'www.hi.com'},
+				  			   {'noise' : False,
+				  			   	'animal' : 'Frog'}
+				  			  ]}
+
+	result_list = al.review_response(dictionary)
+
+	expected_list = [{'title' : u'Nice Title', 'authors' : u'Carlo', 'year' : u'1992', 'link' : 'www.hi.com'}]
+
+	assert_equal(result_list, expected_list, "The obtained response is different from the expected one")
+
+# general test, from the link to the result list given by review_response
+def test_review_response_correct_from_link():
+
+	link = 'http://export.arxiv.org/api/query?search_query=all:electron+AND+all:proton'
+	test_file = 'text_parsing_test.txt'
+
+	response = al.request_to_arxiv(link)
+	parsed_response = al.parse_response(response)
+
+	result_list = al.review_response(parsed_response)
+
+	expected_list = {'link': u'http://arxiv.org/abs/0806.3233v1',
+					 'authors': u"Anatoly Yu. Smirnov, Sergey E. Savel'ev, Franco Nori",
+					 'year': u'2008',
+					 'title': u'Shuttle-mediated proton pumping across the inner mitochondrial membrane'}
+
+	assert_equal(result_list[4], expected_list, "The obtained response is different from the expected one")
+
+# IS FIELD THERE TESTS
+
+# when the filed is not there, al.is_field_there should give None
+def test_is_field_there_no():
+
+	dictionary = {'a' : 1 , 'b' : 'hi'}
+
+	element = al.is_field_there(dictionary,'c')
+
+	assert_equal(element, None, "The obtained response is different from the expected one")
+	
+# when the filed is there, al.is_field_there should give a value
+def test_is_field_there_no():
+
+	dictionary = {'a' : 1 , 'b' : 'hi'}
+
+	element = al.is_field_there(dictionary,'a')
+
+	assert_equal(element, 1, "The obtained response is different from the expected one")
+
+# ONE LINE TITLE TESTS
+
+# when the title field is not there, one_line_title should give None
+def test_one_line_title_no_entry():
+
+	dictionary = {'a' : 1 , 'b' : 'hi'}
+
+	element = al.one_line_title(dictionary)
+
+	assert_equal(element, None, "The obtained response is different from the expected one")
+
+# when the title field is there, but it is not a string
+def test_one_line_title_wrong_entry():
+
+	dictionary = {'a' : 1 , 'b' : 'hi', 'title' : 1}
+
+	element = al.one_line_title(dictionary)
+
+	assert_equal(element, None, "The obtained response is different from the expected one")
+
+# when the title field is there, and there is a string with no \n
+def test_one_line_title_no_newline():
+
+	title_string = u'Once upon a time'
+	dictionary = {'a' : 1 , 'b' : 'hi', 'title' : title_string}
+
+	element = al.one_line_title(dictionary)
+
+	assert_equal(element, title_string, "The obtained response is different from the expected one")
+
+# when the title field is there, and there is a string with \n
+def test_one_line_title_newline():
+
+	title_string = u'Once upon\n a time'
+	expected_string = u'Once upon a time'
+
+	dictionary = {'a' : 1 , 'b' : 'hi', 'title' : title_string}
+
+	element = al.one_line_title(dictionary)
+
+	assert_equal(element, expected_string, "The obtained response is different from the expected one")
+
+# COMPATC AUTHORS TESTS
+
+# when the author field is not there, compact_authors should give None
+def test_compact_authors_no_entry():
+
+	dictionary = {'a' : 1 , 'b' : 'hi'}
+
+	element = al.compact_authors(dictionary)
+
+	assert_equal(element, None, "The obtained response is different from the expected one")
+
+# when the author field is not a list, compact_authors should give None
+def test_compact_authors_no_list():
+
+	dictionary = {'a' : 1 , 'b' : 'hi', 'authors' : 2}
+
+	element = al.compact_authors(dictionary)
+
+	assert_equal(element, None, "The obtained response is different from the expected one")
+
+# when the author field is a list of auhtors, but they do not have the field 'name'
+def test_compact_authors_no_name():
+
+	dictionary = {'a' : 1 , 'b' : 'hi', 'authors' : [{'c' : 1},{'d' : False}] }
+
+	element = al.compact_authors(dictionary)
+
+	assert_equal(element, None, "The obtained response is different from the expected one")
+
+# when the author field is a list of auhtors, each of them with 'name', but they are not strings
+def test_compact_authors_no_strings():
+
+	dictionary = {'a' : 1 , 'b' : 'hi', 'authors' : [{'name' : 1},{'name' : False}] }
+
+	element = al.compact_authors(dictionary)
+
+	assert_equal(element, None, "The obtained response is different from the expected one")
+
+# when the author field is a list of auhtors, each of them with 'name', and a unicode string associated to it
+def test_compact_authors_correct():
+
+	dictionary = {'a' : 1 , 'b' : 'hi',
+			      'authors' : [{'name' : unicode('Carlo Sparaciari', "utf-8")},
+			      			   {'name' : 2},
+			      			   {'title' : False},
+							   {'name' : unicode('Thomas Galley', "utf-8")},
+							   {'name' : 3}
+							  ]
+			     }
+
+	element = al.compact_authors(dictionary)
+
+	expected_element = unicode('Carlo Sparaciari, Thomas Galley' , "utf-8")
+
+	assert_equal(element, expected_element, "The obtained response is different from the expected one")
+
+# FIND YEAR TESTS
+
+# when the date field is not there, find_year should give None
+def test_find_year_no_entry():
+
+	dictionary = {'a' : 1 , 'b' : 'hi'}
+
+	element = al.find_year(dictionary)
+
+	assert_equal(element, None, "The obtained response is different from the expected one")
+
+# when the date field is there, but it is not associated with a string
+def test_find_year_wrong_entry():
+
+	dictionary = {'a' : 1 , 'b' : 'hi', 'date' : 3}
+
+	element = al.find_year(dictionary)
+
+	assert_equal(element, None, "The obtained response is different from the expected one")
+
+# when the date field is there, but it is not associated with a string
+def test_find_year_wrong_length():
+
+	dictionary = {'a' : 1 , 'b' : 'hi', 'date' : u'123'}
+
+	element = al.find_year(dictionary)
+
+	assert_equal(element, None, "The obtained response is different from the expected one")
+
+# when find year works fine
+def test_find_year_correct():
+
+	dictionary = {'a' : 1 , 'b' : 'hi', 'date' : u'1234567'}
+
+	element = al.find_year(dictionary)
+
+	assert_equal(element, u'1234', "The obtained response is different from the expected one")
