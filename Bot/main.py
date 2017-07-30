bfd424f6286d75e1f15b6049b40d1eaa43037ab0
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2017-07-30 21:00:08 +0100

    Main modified to catch exceptions raised by messageloop method.

--- Bot/main.py
+++ Bot/main.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath(os.path.join('..', 'Library')))
 
 import arxiv_bot as ab
 import yaml
+import time
 
 errors_log_file = os.path.join('LogFiles', 'errors.log')
 chat_log_file = os.path.join('LogFiles', 'chat_recorder.log')
@@ -16,4 +17,12 @@ with open(os.path.join('Data','bot_details.yaml'), 'r') as file_input:
 
 bot = ab.ArxivBot(detail['token'])
 bot.set_log_files(errors_log_file, chat_log_file, feedback_log_file)
-bot.message_loop(run_forever = True)
+
+try:
+	bot.message_loop(run_forever = True)
+except:
+	exception_type, exception_description, traceback = sys.exc_info()
+	error_time = time.strftime("%d %b %Y %H:%M:%S", time.localtime())
+	error_string = error_time + ' - MessageLoop exception (Telepot) - ' + exception_type.__name__ + ' - ' + str(exception_description) + '\n'
+	with open(errors_log_file, 'a') as errlog:
+			errlog.write(error_string)
